62e60d36c4d53da3ec59c24cf22df5514b37c8ee
User <davidleetre@gmail.com>
2019-10-07 10:52:09 +0900

    service-worker.js modified

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,11 +1,50 @@
 // service-worker.js
+
+// CacheName
+const cacheName = 'KDKApp_V1';
+
+// Cache Resources
+const cacheResources = [
+  '/' ,
+  './PwaTest.html' ,
+  './js/audio_api.js'
+]
+
+// installイベント：必要なリソースをcacheに投入する
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
+
+  var cachePromise = caches.open(cacheName).then(
+    cache => cache.addAll(cacheResources)
+  );
+  e.waitUntil(cachePromise);
 });
 
+// activeイベント：cacheのkeysにより、cacheのリソースを更新する
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
+
+  var cachePromise = caches.keys().then(
+    keys => Promise.all(keys.map(
+      key => {
+        if( key !== cacheName ){
+          return caches.delete(key);
+        }
+      }))
+    );
+  e.waitUntil(cachePromise);
+  return self.clients.claim();
 });
 
-// 現状では、この処理を書かないとService Workerが有効と判定されないようです
-self.addEventListener('fetch', function(event) {});
+// fetchイベント：リソースをアクセスすると、cacheにある場合、そのまま返す、存在しない場合、WEBサーバーへアクセス
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch');
+
+  e.responseWith(caches.match(e.request).then(
+      cache => cache || fetch(e.request)
+    ).cache(
+      err => {
+      console.log ('cache not exists')
+      return fetch(e.request);
+    }));
+});
