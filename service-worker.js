ccf19ea6f76e3757cdb577f8e302e71a4aa76f16
User <davidleetre@gmail.com>
2019-10-08 14:33:26 +0900

    cache verb control add

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,6 @@
 // service-worker.js
+// version情報
+const swVerb = '1.0';
 
 // workbox-sw.jsをインポート
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
@@ -20,7 +22,7 @@ workbox.core.clientsClaim();
 workbox.routing.registerRoute(
   /\.(js|css)$/,
   new workbox.strategies.StaleWhileRevalidate({
-    cacheName: 'cache-web-resources',
+    cacheName: 'cache-web-resources-v' + swVerb,
   })
 );
 
@@ -28,7 +30,7 @@ workbox.routing.registerRoute(
 workbox.routing.registerRoute(
   /\.(html|xhtml)$/,
   new workbox.strategies.NetworkFirst({
-    cacheName: 'cache-web-pages'
+    cacheName: 'cache-web-pages-v' + swVerb
   })
 );
 
@@ -36,7 +38,7 @@ workbox.routing.registerRoute(
 workbox.routing.registerRoute(
   /\.(?:png|gif|jpg|jpeg|webp|svg)$/,
   new workbox.strategies.CacheFirst({
-    cacheName: 'cache-web-images',
+    cacheName: 'cache-web-images-v' + swVerb,
     plugins: [
       new workbox.expiration.Plugin({
         maxEntries: 120, // 最大ファイル数：120
@@ -47,6 +49,32 @@ workbox.routing.registerRoute(
 );
 /*******************リソースのキャッシュルール設定完了**********************/
 
+// activeイベント：古いcacheを削除する
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activated');
+
+  var activatePromise = caches.keys().then(
+    keys => Promise.all(keys.map(
+      key => {
+        if( key.includes('cache-web-') && !key.includes(swVerb) ){
+          return caches.delete(key);
+        } else {
+          return Promise.resolve();
+        }
+      }))
+    );
+  e.waitUntil(activatePromise);
+  return self.clients.claim();
+});
+/*
+self.addEventListener('fetch', function(e) {
+
+  const requestURL = new URL(e.request.url);
+  if (!e.request.referrer.includes(requestURL.hostname)) {
+    return e.respondWith(fetch(e.request));
+  }
+})
+*/
 /*
 // ドメイン名称
 var domainNM = 'davidleetre.github.io';
