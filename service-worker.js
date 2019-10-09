e5b112e6ecd11dba1e1790ec22bf482793a185d2
User <davidleetre@gmail.com>
2019-10-09 13:51:35 +0900

    verb control

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 // version情報
-const swVerb = '3.1';
+const swVerb = localStorage.getItem('sw_version');
 
 // workbox-sw.jsをインポート
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
