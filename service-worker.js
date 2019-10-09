beb006e8131031a9d0d0dcb9bdb75808288a20ef
User <davidleetre@gmail.com>
2019-10-09 10:28:12 +0900

    version up

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 // version情報
-const swVerb = '2.6';
+const swVerb = '2.7';
 
 // workbox-sw.jsをインポート
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
